93a83240a2c5d195fd149bace059a8e9c1aaa94b
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-10-28 14:24:10 +0900

    Add toppage and aboutpage

---
+++ src/common/constants.js
@@ -0,0 +1,24 @@
+export default {
+  menuItems: [
+    {
+      name: 'HOME',
+      url: '/'
+    },
+    {
+      name: 'ABOUT',
+      url: '/about'
+    },
+    {
+      name: 'WORK',
+      url: '/work'
+    },
+    {
+      name: 'BLOG',
+      url: '/blog'
+    },
+    {
+      name: 'ACCESS',
+      url: '/access'
+    }
+  ]
+}
---
+++ src/components/AppBackgroundHolder.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="background-holder">
+        <v-container fluid fill-height class="background-holder__content">
+            <v-row>
+                <v-col class="background-holder__content-text">
+                    <p><strong>{{ title  }}</strong></p>
+                </v-col>
+            </v-row>
+        </v-container>
+    </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.background-holder__content {
+    background: url("../assets/img/universal.jpg");
+    background-size: cover;
+    background-position: center center;
+    width: 100%;
+    height: 30vh;
+
+    &-text {
+        font-family: 'Londrina Outline', cursive;
+        color: white;
+        text-align: center;
+        font-size: 28px;
+        font-weight: bold;
+
+        @include display_pc {
+            font-size: 40px;
+        }
+
+    }
+    strong {
+        background: linear-gradient(transparent 50%, rgba(0, 0, 0, 0.6));
+    }
+}
+</style>
